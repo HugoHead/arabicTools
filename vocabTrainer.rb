e636808734ab671ad1d6e5fd48226aeb786ceee3
HugoHead <hats1234.hats1234@gmail.com>
2021-10-06 22:44:02 -0400

    now more crossplatform

--- vocabTrainer.rb
+++ vocabTrainer.rb
@@ -1,38 +1,23 @@
- def OS.windows?
-   (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
- end
-
- def OS.mac?
-  (/darwin/ =~ RUBY_PLATFORM) != nil
- end
-
- def OS.unix?
-   !OS.windows?
- end
-
- def OS.linux?
-   OS.unix? and not OS.mac?
- end
-
- def OS.jruby?
-   RUBY_ENGINE == 'jruby'
- end
-
-#
-#
-# gem install win32-sound
 require "tty-reader"
-
-if OS.windows?
+$windows = false
+if (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
   require 'win32/sound'
   include Win32
+  $windows = true
 end
 
+def playSound file
+  if $windows
+      Sound.play(file, Sound::ALIAS)
+  else
+      system("aplay #{file}")
+  end
+end
 
 audioFilList = ARGV
 audioFileList = audioFilList.shuffle
 audioFilList.each do |wordAudioFile|
-	Sound.play(wordAudioFile, Sound::ALIAS)
+	playSound(wordAudioFile)
 	puts "What word was just said?"
 	response = gets.chomp("\n")
 	puts response == "ا"
